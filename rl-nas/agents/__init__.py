070807519d95a6832873e6d1c58bcc0a8a959489
RL-NAS-TSF4E <168930273+RL-NAS-TSF4E@users.noreply.github.com>
2024-05-09 21:39:09 +0200

    Update __init__.py

--- rl-nas/agents/__init__.py
+++ rl-nas/agents/__init__.py
@@ -1,2 +1,2 @@
-from rl-nas.agents.a2c_agent import A2CAgent
-from rl-nas.agents.ppo_agent import PPOAgent
+from rl_nas.agents.a2c_agent import A2CAgent
+from rl_nas.agents.ppo_agent import PPOAgent
